f5957072449705b2738cb12e93ff40fc6c6ff130
Tommy Yu <tommy.yu@auckland.ac.nz>
2022-03-25 03:07:59 +1300

    Refactored the sparc convert utility
    - This is now the base utility with a method to be implemented that will
      invoke the external program, defined by attributes set in subclass.

--- fieldml/pmr2/utility.py
+++ fieldml/pmr2/utility.py
@@ -49,10 +49,15 @@ class ZincJSUtility(object):
         p.communicate(model_data)
 
 
-@zope.interface.implementer(ISparcConvertUtility)
-class SparcConvertUtility(object):
+class SparcUtilityBase(object):
+    """
+    Common methods that deal with sparc programs and their input files.
+    """
 
-    def get_paths(self, neon_input):
+    executable_key = None
+    binary_name = None
+
+    def get_paths(self, sparc_input):
         def get_region_model_sources(region):
             sources = region.get('Model', {}).get('Sources', [])
             results = {
@@ -64,8 +69,8 @@ class SparcConvertUtility(object):
             return results
 
         try:
-            neon_doc = json.loads(neon_input)
-            return get_region_model_sources(neon_doc['RootRegion'])
+            sparc_doc = json.loads(sparc_input)
+            return get_region_model_sources(sparc_doc['RootRegion'])
             # watch out for absolute paths
         except (KeyError, TypeError, ValueError, AttributeError):
             return []
@@ -82,7 +87,8 @@ class SparcConvertUtility(object):
             with open(fullpath, 'wb') as fd:
                 fd.write(data)
 
-    def __call__(self, working_dir, storage, neon_input):
+    def __call__(
+            self, working_dir, storage, sparc_input, rmtree_tmp=False, **kw):
         registry = zope.component.getUtility(IRegistry)
         try:
             settings = registry.forInterface(ISettings, prefix=prefix)
@@ -93,34 +99,52 @@ class SparcConvertUtility(object):
             )
             return
 
-        executable = find_executable(settings.sparc_convert)
+        executable = find_executable(getattr(settings, self.executable_key))
         if executable is None:
             logger.warning(
-                'unable to find the sparc-convert binary; please '
+                'unable to find the %s binary; please '
                 "verify the registry key '%s' is set to the valid binary",
-                prefix
+                self.binary_name,
+                prefix,
             )
             return
 
         # figure out what paths to extract
-        paths = self.get_paths(neon_input)
+        paths = self.get_paths(sparc_input)
 
         # create a temporary directory in working_dir to write out; this
         # is currently done in the working_dir which is assumed to be
         # freshly created by the caller of this function.
         tmpdir = join(working_dir, 'src')
         os.mkdir(tmpdir)  # so this should always succeed
-        # the neon file
-        neon_path = join(tmpdir, 'input.neon')
-        with open(neon_path, 'w') as fd:
-            fd.write(neon_input)
+        # the sparc related file
+        sparc_path = join(tmpdir, self.sparc_filename)
+        with open(sparc_path, 'w') as fd:
+            fd.write(sparc_input)
 
         self.extract_paths(tmpdir, storage, paths)
 
-        # then invoke the process;
         # restrict env to just the bare minimum, i.e. don't let things
         # like PYTHONPATH (if set) to interfere with the calling.
-        # TODO need to extract Sources
         env = {k: os.environ[k] for k in ('PATH',)}
-        call([executable, 'web-gl', neon_path], env=env, cwd=working_dir)
-        # TODO cleanup the temporary files?
+
+        # then invoke the process;
+        self.call(executable, sparc_path, env, working_dir, **kw)
+
+        # TODO default the following to true so that the cleanup is done
+        if rmtree_tmp:
+            rmtree(tmpdir)
+
+    def call(self, executable, sparc_path, env, working_dir, **kw):
+        raise NotImplementedError
+
+
+@zope.interface.implementer(ISparcConvertUtility)
+class SparcConvertUtility(SparcUtilityBase):
+
+    executable_key = 'sparc_convert'
+    binary_name = 'sparc-convert'
+    sparc_filename = 'input.neon'
+
+    def call(self, executable, sparc_path, env, working_dir, **kw):
+        call([executable, 'web-gl', sparc_path], env=env, cwd=working_dir)
